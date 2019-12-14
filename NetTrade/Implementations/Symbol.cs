041e605da47054eb698339a707a0d21b136f3a43
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2019-12-14 14:17:21 +0300

    Added ISymbol implementation

---
+++ NetTrade/Implementations/Symbol.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NetTrade.Interfaces;
+
+namespace NetTrade.Implementations
+{
+    public class Symbol : ISymbol
+    {
+        public Symbol()
+        {
+            Bars = new Bars();
+        }
+
+        public string DataFilePath { get; set; }
+
+        public string Name { get; set; }
+
+        public double TickSize { get; set; }
+
+        public double Commission { get; set; }
+
+        public int Digits { get; set; }
+
+        public long MinVolume { get; set; }
+
+        public long MaxVolume { get; set; }
+
+        public long VolumeStep { get; set; }
+
+        public IBars Bars { get; }
+    }
+}
