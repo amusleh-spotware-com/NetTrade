dd731d56d0470429dab03d41ccc89ec79e091f09
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2020-03-09 13:20:17 +0300

    ExponentialMovingAverage added

---
+++ src/NetTrade/Indicators/ExponentialMovingAverage.cs
@@ -0,0 +1,58 @@
+﻿using NetTrade.Abstractions.Interfaces;
+using NetTrade.Abstractions;
+using NetTrade.Collections;
+using NetTrade.Enums;
+using System.Linq;
+
+namespace NetTrade.Indicators
+{
+    public class ExponentialMovingAverage : Indicator
+    {
+        private ExpandableSeries<double> _data = new ExpandableSeries<double>();
+
+        public ExponentialMovingAverage(ISymbol symbol, int periods, DataSourceType dataSourceType)
+        {
+            Symbol = symbol;
+
+            Symbol.IndicatorOnBarEvent += Symbol_OnBarEvent;
+
+            Periods = periods;
+
+            DataSourceType = dataSourceType;
+
+            WeightedMultiplier = 2 / (Periods + 1.0);
+        }
+
+        public ISymbol Symbol { get; }
+
+        public double WeightedMultiplier { get; }
+
+        public int Periods { get; }
+
+        public DataSourceType DataSourceType { get; }
+
+        public ISeries<double> Data => _data;
+
+        private void Symbol_OnBarEvent(object sender, int index)
+        {
+            var symbolData = Symbol.Bars.GetData(DataSourceType);
+
+            var dataPoint = double.NaN;
+
+            if (index == 0)
+            {
+                dataPoint = symbolData[index];
+            }
+            else
+            {
+                var previousEma = _data[index - 1];
+
+                dataPoint = (symbolData[index] - previousEma) * WeightedMultiplier + previousEma;
+            }
+
+            _data.Add(dataPoint);
+
+            OnNewValue(index);
+        }
+    }
+}
